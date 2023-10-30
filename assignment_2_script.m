628f11c440f4029b39b885c16e847b385fac1c6a
UshnishChowdhury <chowdhury.ushnish1995@gmail.com>
2023-10-30 11:57:31 +0200

    Successfuly testing the model and plotting results through matlab

--- assignment_2_script.m
+++ assignment_2_script.m
@@ -1,1 +1,10 @@
-simulink
+%simulink
+
+subplot(2,1,1); % Divides the figure to two subplots
+plot(i.time, i.data); grid on;
+%i N = 20; % Rated current
+%plot(i.time,i.data/i N); % This would plot the p.u. current
+xlabel('Time (s)'); ylabel('Current (A)');
+subplot(2,1,2);
+plot(w_M.time,w_M.data); grid on;
+xlabel('Time (s)'); ylabel('Speed (rad/s)');
