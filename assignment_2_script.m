03b7e63e054ebb2afb845bcfadeffe036f676ab1
UshnishChowdhury <chowdhury.ushnish1995@gmail.com>
2023-10-31 09:41:08 +0200

    Testing the motor model and saving the 1st plot

--- assignment_2_script.m
+++ assignment_2_script.m
@@ -1,10 +1,21 @@
 %simulink
 
-subplot(2,1,1); % Divides the figure to two subplots
-plot(i.time, i.data); grid on;
+% Divides the figure to two subplots
+subplot(2,1,1);
+
+%Plotting output current
+plot(i.time, i.data);
+grid on;
+xlabel('Time (s)'); 
+ylabel('Current (A)');
+
 %i N = 20; % Rated current
 %plot(i.time,i.data/i N); % This would plot the p.u. current
-xlabel('Time (s)'); ylabel('Current (A)');
+
 subplot(2,1,2);
-plot(w_M.time,w_M.data); grid on;
-xlabel('Time (s)'); ylabel('Speed (rad/s)');
+
+%Plotting output rotational speed
+plot(w_M.time,w_M.data); 
+grid on;
+xlabel('Time (s)'); 
+ylabel('Speed (rad/s)');
