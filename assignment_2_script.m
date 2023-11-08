e0dc0ec29ad060efcaa4cebbf30f020d199b74ab
UshnishChowdhury <chowdhury.ushnish1995@gmail.com>
2023-11-08 14:39:53 +0200

    Saving the DC Motor model as pdf

--- assignment_2_script.m
+++ assignment_2_script.m
@@ -1,21 +1,25 @@
 %simulink
 
+% Rated Current 
+i_N = 20;
+
+% Rated rotational speed
+w_N = 3000 * (2*pi/60);
+
+
 % Divides the figure to two subplots
 subplot(2,1,1);
 
-%Plotting output current
-plot(i.time, i.data);
+%Plotting output current per unit
+plot(i.time, i.data / i_N);
 grid on;
 xlabel('Time (s)'); 
-ylabel('Current (A)');
-
-%i N = 20; % Rated current
-%plot(i.time,i.data/i N); % This would plot the p.u. current
+ylabel('pu Current (i / i_N)');
 
 subplot(2,1,2);
 
-%Plotting output rotational speed
-plot(w_M.time,w_M.data); 
+%Plotting output rotational speed per unit
+plot(w_M.time, w_M.data / w_N); 
 grid on;
 xlabel('Time (s)'); 
-ylabel('Speed (rad/s)');
+ylabel('pu Speed (w_M / w_N)');
