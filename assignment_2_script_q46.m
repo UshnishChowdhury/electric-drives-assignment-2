45b9f6cdc3fcb76bd25338d4161f597af0260b1b
UshnishChowdhury <chowdhury.ushnish1995@gmail.com>
2023-11-09 01:51:47 +0200

    Adding the current controller for Task 4 and 5

---
+++ assignment_2_script_q46.m
@@ -0,0 +1,15 @@
+subplot(2, 1, 1)
+plot(i.time, i.data); 
+grid on;
+hold on;
+stairs(i_k.time, i_k.data, 'r');    % Discrete signal
+axis([0.15 0.1504 9 11]);           % Controls axis scaling
+xlabel('Time (s)'); 
+ylabel('Current (A)');
+
+subplot(2, 1, 2)
+plot(u.time, u.data); 
+grid on;
+axis([0.15 0.1504 -10 150]);
+xlabel('Time (s)'); 
+ylabel('Voltage (V)');
