892f5910d4d7484dbfdead94608918bc484479d4
UshnishChowdhury <chowdhury.ushnish1995@gmail.com>
2023-11-10 14:55:13 +0200

    Modelling the DC Motor with 2DOF PI current and Speed controller

--- assignment_2_script_q6.m
+++ assignment_2_script_q6.m
@@ -5,21 +5,23 @@ L = 2.5e-3;                 % Inductance
 k = 0.35;                   % Flux factor
 J = 1e-3;                   % Moment of inertia
 %% Gains of the 2DOF PI current controller
-alpha_c = 2*pi*400;         % Closed−loop bandwidth
-k_p = 2*alpha_c*L - R;     % Proportional gain
-k_i = alpha_c^2*L;         % Integral gain
-k_t = alpha_c*L;           % Reference feedforward gain
+
+alpha_c = 2* pi* 400;         % Closed−loop bandwidth
+k_p = 2 * alpha_c* L - R;     % Proportional gain
+k_i = alpha_c^2 * L;         % Integral gain
+k_t = alpha_c * L;           % Reference feedforward gain
 u_min = -140;               % Saturation: lower limit
 u_max = 140;                % Saturation: upper limit
 T_sw = 200e-6;
-%% Gains of the 2DOF PI speed controller (for Section 4.2)
-%alpha s = 0.1*alpha c; % Closed−loop bandwidth
-%k ps = ...; % Proportional gain
-%k is = ...; % Integral gain
-%k ts = ...; % Reference feedforward gain
-%tau N = 7; % Rated torque
-%tau min = −2*tau N; % Saturation: lower limit
-%tau max = 2*tau N; % Saturation: upper limit
+%% Gains of the 2DOF PI speed controller
+
+alpha_s = 0.1 * alpha_c;      % Closed−loop bandwidth
+k_ps = 2 * alpha_s * J;       % Proportional gain
+k_is = alpha_s^2 * J;         % Integral gain
+k_ts = alpha_s * J;           % Reference feedforward gain
+tau_N = 7;                    % Rated torque
+tau_min = -2*tau_N;           % Saturation: lower limit
+tau_max = 2*tau_N;            % Saturation: upper limit
 
 subplot(2, 1, 1)
 plot(tau_M.time, tau_M.data, 'LineWidth', 1);
