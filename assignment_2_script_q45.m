705185e7563859a3eb98c9b453c9348a24359dde
UshnishChowdhury <chowdhury.ushnish1995@gmail.com>
2023-11-19 22:36:12 +0200

    Fine tuning all plots

--- assignment_2_script_q45.m
+++ assignment_2_script_q45.m
@@ -6,10 +6,16 @@ stairs(i_k.time, i_k.data, 'r');    % Discrete signal
 axis([0.15 0.1504 9 11]);           % Controls axis scaling
 xlabel('Time (s)'); 
 ylabel('Current (A)');
+legend('Actual Current', 'Synchronously Sampled Current');
+title('Current as a function of Time');
 
 subplot(2, 1, 2)
 plot(u.time, u.data);
 grid on;
+hold on;
+plot(u_ref.time, u_ref.data);
 axis([0.15 0.1504 -10 150]);
 xlabel('Time (s)'); 
 ylabel('Voltage (V)');
+legend('Actual Voltage', 'Reference Voltage');
+title('Voltage as a function of Time');
--- assignment_2_script_q710.m
+++ assignment_2_script_q710.m
@@ -1,6 +1,8 @@
 %% Parameter estimates
-R = 0.5;                    % Resistance
-L = 2.5e-3;                 % Inductance
+% R = 0.5;                    % Resistance
+R = 0.5 * 1.5; 
+% L = 2.5e-3;                 % Inductance
+L = 2.5e-3 * 0.7;
 k = 0.35;                   % Flux factor
 J = 1e-3;                   % Moment of inertia
 %% Gains of the 2DOF PI current controller
@@ -14,36 +16,93 @@ u_max = 140;                % Saturation: upper limit
 T_sw = 200e-6;
 %% Gains of the 2DOF PI speed controller
 
-alpha_s = 0.1 * alpha_c;      % Closed−loop bandwidth
+alpha_s = 0.1 * alpha_c;       % Closed−loop bandwidth
 %k_ps = 2 * alpha_s * J;       % Proportional gain
-k_ps = alpha_s * J;          % Proportional gain
+k_ps = alpha_s * J;            % Proportional gain
 %k_is = alpha_s^2 * J;         % Integral gain
-k_is = -1;
-k_ts = alpha_s * J;           % Reference feedforward gain
+k_is = 0;
+%k_ts = alpha_s * J;           % Reference feedforward gain
+k_ts = k_ps;
 tau_N = 7;                    % Rated torque
 tau_min = -2*tau_N;           % Saturation: lower limit
 tau_max = 2*tau_N;            % Saturation: upper limit
 
-%% Code for plotting results
+%% Code for plotting results (Torque and Rotational Speed)
 figure;
-subplot(2, 1, 1)
-plot(i.time, i.data, 'color', 'b');
+subplot(3, 1, 1)
+plot(i.time, i.data, 'color', 'b', 'LineWidth', 1);
 grid on;
 hold on;
 % Reference signal
 stairs(i_ref.time, i_ref.data, 'black');
-axis([0 0.5 -15 15]);
-title("Torque as a function of time");
+axis([0 0.5 -50 50]);
+title("Current as a function of time");
 xlabel('Time (s)'); 
-ylabel('Torque (Nm)');
+ylabel('Current (A)');
 
-subplot(2, 1, 2)
-plot(w_M.time, w_M.data, 'color', 'r');
+subplot(3, 1, 2)
+plot(w_M.time, w_M.data, 'color', 'r', 'LineWidth', 1);
 grid on;
 hold on;
 % Reference signal
 stairs(w_M_ref.time, w_M_ref.data, 'black');
-axis([0 0.5 -160 200]);
+axis([0 0.5 -200 200]);
+title("Rotational Speed as a function of time");
+xlabel('Time (s)'); 
+ylabel('Speed (rad / sec)');
+
+subplot(3, 1, 3)
+plot(u.time, u.data, 'color', 'black', 'LineWidth', 1);
+grid on;
+axis([0 0.5 -200 200]);
+title("Voltage as a function of time");
+xlabel('Time (s)'); 
+ylabel('Voltage (V)');
+
+%% Code for plotting results (Torque and Rotational Speed)
+figure;
+subplot(2, 1, 1)
+plot(i.time, i.data, 'color', 'b', 'LineWidth', 1);
+grid on;
+hold on;
+% Reference signal
+stairs(i_ref.time, i_ref.data, 'black');
+axis([0 0.5 -50 50]);
+title("Current as a function of time");
+xlabel('Time (s)'); 
+ylabel('Current (A)');
+
+subplot(2, 1, 2)
+plot(w_M.time, w_M.data, 'color', 'r', 'LineWidth', 1);
+grid on;
+hold on;
+% Reference signal
+stairs(w_M_ref.time, w_M_ref.data, 'black');
+axis([0 0.5 -200 200]);
+title("Rotational Speed as a function of time");
+xlabel('Time (s)'); 
+ylabel('Speed (rad / sec)');
+
+%% Code for plotting results (Torque and Rotational Speed)
+figure;
+subplot(2, 1, 1)
+plot(i.time, i.data, 'color', 'b', 'LineWidth', 1);
+grid on;
+hold on;
+% Reference signal
+stairs(i_ref.time, i_ref.data, 'black');
+axis([0.1 0.37 -1 50]);
+title("Current as a function of time");
+xlabel('Time (s)'); 
+ylabel('Current (A)');
+
+subplot(2, 1, 2)
+plot(w_M.time, w_M.data, 'color', 'r', 'LineWidth', 1);
+grid on;
+hold on;
+% Reference signal
+stairs(w_M_ref.time, w_M_ref.data, 'black');
+axis([0.1 0.37 -400 0]);
 title("Rotational Speed as a function of time");
 xlabel('Time (s)'); 
 ylabel('Speed (rad / sec)');
